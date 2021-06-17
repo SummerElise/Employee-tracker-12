92a8dde525348c1f1844a20a0199058cc82c0915
lissy <slissy513@gmail.com>
2021-06-17 12:57:38 -0400

    added schema.sql file as well as seed.sql file and created tables in mysql

---
+++ index.js
@@ -0,0 +1,15 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'employeeTracker_DB',
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    runSearch();
+});
